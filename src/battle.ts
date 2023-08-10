b54bfb2df615e2c55f4da0acb08bd3a1d7836d93
suganya05 <101466647+suganya05@users.noreply.github.com>
2023-08-10 11:17:29 +0530

    feat: createBattle, endBattle, bedBattle create

--- src/battle.ts
+++ src/battle.ts
@@ -1,3 +1,66 @@
-import { _CreateBattle } from "../generated/Battle/Battle";
+import {
+  _BattleStateChanged,
+  _BetBattle,
+  _CreateBattle,
+} from "../generated/Battle/Battle";
+import { Battle, Participant } from "../generated/schema";
 
-export function handle_CreateBattle(event: _CreateBattle): void {}
+export function handleCreateBattle(event: _CreateBattle): void {
+  let battle = new Battle(event.params._battleId.toString());
+
+  battle.battleId = event.params._battleId;
+  battle.exchange = event.params.exchange;
+  battle.startTime = event.params.startTime;
+  battle.endTime = event.params.endTime;
+  battle.entryfee = event.params.entryFee;
+  battle.nftCount = event.params.nftCount;
+  battle.participants = [];
+  battle.owner = event.params.owner.toHexString();
+  battle.battleType =
+    event.params.battleType.toString() == "0" ? "Health" : "Blood";
+  battle.battleStatus = "Betting";
+
+  battle.save();
+}
+
+export function handleBetBattle(event: _BetBattle): void {
+  let participant = new Participant(event.transaction.hash.toString());
+
+  participant.battleId = event.params._battleId;
+  participant.user = event.params._playerAddress;
+  participant.billionsIds = event.params._nftIds;
+  participant.scalarIds = event.params._scalarIds;
+  participant.save();
+
+  let battle = Battle.load(event.params._battleId.toString());
+
+  if (!battle) return;
+
+  let newParticipants = battle.participants;
+  newParticipants.push(participant.id);
+  battle.participants = newParticipants;
+  battle.save();
+}
+
+export function handleBattleStateChanged(event: _BattleStateChanged): void {
+  let battle = Battle.load(event.params._battleId.toString());
+
+  if (!battle) return;
+
+  let battleStatus = event.params.state.toString();
+
+  if (battleStatus == "0") {
+    battle.battleStatus = "Betting";
+  }
+  if (battleStatus == "1") {
+    battle.battleStatus = "Started";
+  }
+  if (battleStatus == "2") {
+    battle.battleStatus = "Ended";
+  }
+  if (battleStatus == "3") {
+    battle.battleStatus = "Expired";
+  }
+
+  battle.save();
+}
